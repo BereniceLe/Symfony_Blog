82d9534fcb197fa83e400f0fa405747f3af4334a
Lemoine Berenice <berenice.lemoine@etu.umontpellier.fr>
2020-11-05 18:01:53 +0100

    Début de template

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -20,7 +20,13 @@ class BlogController extends AbstractController
     */
     public function view_post(int $id): Response
     {
-      return $this->render('post.html.twig',['id' => $id]);
+      $articles = [["title" => "Titre de l'article", "description" => "Description de l'article",
+      "date" => "5 Novembre 2020", "body" => "Cet article est une présentation d'un article. C'est un petit exemple de test tout pourri mais bon faut bien commencer quelque part. :)","genre" => " Test"],
+      ["title" => "Titre de l'article 2", "description" => "Description de l'article",
+      "date" => "5 Octorbre 2020", "body" => "Cet article est une présentation d'un article. C'est un petit exemple de test tout pourri mais bon faut bien commencer quelque part. :)","genre" => " Test"],
+      ["title" => "Titre de l'article 3", "description" => "Description de l'article",
+      "date" => "4 Novembre 2020", "body" => "Cet article est une présentation d'un article. C'est un petit exemple de test tout pourri mais bon faut bien commencer quelque part. :)","genre" => " Test"]];
+      return $this->render('post.html.twig',['id' => $id, 'articles' => $articles]);
     }
 }
 
